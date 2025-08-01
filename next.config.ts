c1cfdb88ed7b3a5c7fdbe54ec5b2371bb2033624
Tzu-Han Lin <hhtzuhh@gmail.com>
2025-08-01 19:38:31 -0400

    Update Next.js configuration to set basePath and assetPrefix for portfolio deployment

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
     unoptimized: true,
   },
-  // Uncomment the following lines if your repo name is not the same as your username
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/',
 };
 
 export default nextConfig;
