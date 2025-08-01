44e1d6d4f367646c73d71d68898c1b7972e1416b
Tzu-Han Lin <hhtzuhh@gmail.com>
2025-08-01 19:25:33 -0400

    Update Next.js configuration to enable trailing slashes and modify GitHub Actions workflow for improved deployment steps, including Node.js and Pages setup

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  // Uncomment the following lines if your repo name is not the same as your username
+  // basePath: '/your-repo-name',
+  // assetPrefix: '/your-repo-name/',
 };
 
 export default nextConfig;
