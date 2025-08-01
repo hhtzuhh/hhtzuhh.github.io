c801e54c33b62dcda998bcbb27a266a88a7d706e
Tzu-Han Lin <hhtzuhh@gmail.com>
2025-08-01 18:43:15 -0400

    Remove basePath from Next.js config and add GitHub Actions workflow for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   output: 'export',
-  // If your GitHub Pages URL is https://<username>.github.io/<repo-name>
-  // set basePath to '/<repo-name>'
-  basePath: '/portfolio',
   images: {
     unoptimized: true,
   },
